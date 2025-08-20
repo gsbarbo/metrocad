d41796a40e3c7d0f60a5f36ff4a732eda998d9be
Gage B. <gagebarbour96@gmail.com>
2025-08-20 05:54:22 -0500

    Fix error with middleware

--- app/Http/Middleware/Mdt/ActiveUnitAutoOffDutyMiddleware.php
+++ app/Http/Middleware/Mdt/ActiveUnitAutoOffDutyMiddleware.php
@@ -13,10 +13,29 @@ class ActiveUnitAutoOffDutyMiddleware
     {
         $active_units = ActiveUnit::without(['officer', 'user_department', 'calls', 'user'])->get(['id', 'updated_at']);
 
+        \Log::info('ActiveUnit timeout value', [
+            'value' => get_setting('mdt.activeUnitTimeout'),
+            'type' => gettype(get_setting('mdt.activeUnitTimeout')),
+        ]);
+
         foreach ($active_units as $active_unit) {
-            if (Carbon::parse($active_unit->updated_at)->lt(
-                Carbon::now()->subMinutes(get_setting('mdt.activeUnitTimeout'))
-            )) {
+            $timeout = get_setting('mdt.activeUnitTimeout');
+
+            \Log::info('Timeout before subMinutes', [
+                'value' => $timeout,
+                'type' => gettype($timeout),
+                'class' => is_object($timeout) ? get_class($timeout) : null,
+            ]);
+
+            \Log::info('ActiveUnit updated_at', [
+                'raw' => $active_unit->getRawOriginal('updated_at'),
+                'cast' => $active_unit->updated_at,
+                'type' => gettype($active_unit->updated_at),
+            ]);
+
+            $cutoff = Carbon::now()->subMinutes((int) $timeout);
+
+            if (Carbon::parse($active_unit->updated_at)->lt($cutoff)) {
                 $active_unit->update(['off_duty_type_id' => 3]);
                 $active_unit->delete();
             }
